69a4de16db8ee16a255fb349a05f711a6a6a7344
Soumi Maity <128630285+SoumiMaity58@users.noreply.github.com>
2023-07-28 02:45:39 +0530

    Add files via upload

---
+++ count.js
@@ -0,0 +1,13 @@
+let count = 0;
+document.getElementById("dec").onclick = function () {
+    count -= 1;
+    document.getElementById("count").innerHTML = count;
+}
+document.getElementById("res").onclick = function () {
+    count = 0;
+    document.getElementById("count").innerHTML = count;
+}
+document.getElementById("inc").onclick = function () {
+    count += 1;
+    document.getElementById("count").innerHTML = count;
+}
